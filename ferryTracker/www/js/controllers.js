8e72648355ca2dc6ee503aaa8c97ac9fc37068a7
kumarm <manish.kumar05@nagarro.com>
2015-06-06 21:49:39 +0530

    Work for the day

--- ferryTracker/www/js/controllers.js
+++ ferryTracker/www/js/controllers.js
@@ -1,47 +1,57 @@
 var ferryTracker = angular.module('myapp.controllers', [])
 
-ferryTracker.controller('DashCtrl', function ($scope) {
-  $scope.data = {
-    buildings: {
-      "371": ["A", "B", "C"],
-      "36/37": ["A", "C", "D", "E"],
-      "14": ["A", "C", "D", "E"],
-      "GP-28": ["A", "B"],
-      "17/18": ["D"],
-      "13": ["B", "E"]
-    },
-    routes: {
-      "A": ["371", "36/37", "14", "GP-28"],
-      "B": ["371", "GP-28", "13"],
-      "C": ["371", "14", "36/37"],
-      "D": ["36/37", "14", "17/18"],
-      "E": ["36/37", "14", "13"]
-    }
-  }; 
+ferryTracker.controller('DashCtrl', function ($scope, Ferry) {
+    $scope.data = {
+        routes: Ferry.routes(),
+        buildings: Ferry.buildings(),
+        userBuilding: "",
+        buildingRoutes: []
+    };
+
+    $scope.selectBuilding = function () {
+        $scope.data.buildingRoutes = $scope.data.buildings[$scope.data.userBuilding];
+        console.log($scope.data.buildingRoutes);
+    };
 });
 
 ferryTracker.controller('RoutesCtrl', function ($scope, Ferry) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  $scope.data = {
-    routes: Ferry.routes()
-  };
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    $scope.data = {
+        routes: Ferry.routes()
+    };
 });
 
-ferryTracker.controller('RouteDetailCtrl', function ($scope, $stateParams, Ferry) { 
-  $scope.data = {
-    routeId: ($stateParams.routeId).toUpperCase(),
-    routeInfo: Ferry.getRoute($stateParams.chatId)
-  };
+ferryTracker.controller('RouteDetailCtrl', function ($scope, $stateParams, $window, Ferry) {
+    var routeId = ($stateParams.routeId).toUpperCase(),
+        routeArray = Ferry.getRoute(routeId),
+        geoPositions = Ferry.geoPositions(),
+        staticMapPath = "&path=color:0x0000ff|weight:5",
+        staticMapHTML = "https://maps.googleapis.com/maps/api/staticmap?size=" + ($window.innerWidth - 20) + "x" + ($window.innerHeight - 40) + "&scale=1&maptype=roadmap";
+
+    angular.forEach(routeArray, function (building, index) {
+        var geoLocation = geoPositions[building];
+        staticMapHTML += "&markers=size:mid|color:" + geoLocation.marker + "|label:N|" + geoLocation.latitude + "," + geoLocation.longitude;
+        staticMapPath += "|" + geoLocation.latitude + "," + geoLocation.longitude;
+    });
+
+    staticMapHTML += staticMapPath;
+    console.log(staticMapHTML);
+
+    $scope.data = {
+        selectedRoute: routeId,
+        geoPositions: geoPositions,
+        staticMapHTML: staticMapHTML
+    };
 });
 
 ferryTracker.controller('AccountCtrl', function ($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
--- ferryTracker/www/js/services.js
+++ ferryTracker/www/js/services.js
@@ -1,50 +1,87 @@
 var myAppServices = angular.module('myapp.services', [])
 
 myAppServices.factory('Ferry', function () {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var routes = {
-    "A": {
-      "371": {
-        "longitude": "x",
-        "latitude": "y"
-      }, 
-      "36/37": {
-        "longitude": "x",
-        "latitude": "y"
-      }, 
-      "14": {
-        "longitude": "x",
-        "latitude": "y"
-      }, 
-      "GP-28": {
-        "longitude": "x",
-        "latitude": "y"
-      }
-    },
-    "B": {
-      "371": {
-        
-      }, 
-      "GP-28": {
-        
-      }, 
-      "13": {
-        
-      }
-    },
-    "C": ["371", "14", "36/37"],
-    "D": ["36/37", "14", "17/18"],
-    "E": ["36/37", "14", "13"]
-  };
+    var geoPositions = {
+        "371": {
+            "marker": "orange",
+            "latitude": "28.5029773",
+            "longitude": "77.0706075"
+        },
+        "36/37": {
+            "marker": "green",
+            "latitude": "28.5013721",
+            "longitude": "77.0700469"
+        },
+        "14": {
+            "marker": "purple",
+            "latitude": "28.499069",
+            "longitude": "77.0697146"
+        },
+        "GP-28": {
+            "marker": "blue",
+            "latitude": "28.5029773",
+            "longitude": "77.0706075"
+        },
+        "17/18": {
+            "marker": "brown",
+            "latitude": "28.5013721",
+            "longitude": "77.0700469"
+        },
+        "13": {
+            "marker": "red",
+            "latitude": "28.499069",
+            "longitude": "77.0697146"
+        }
+    };
 
-  return {
-    routes: function () {
-      return routes;
-    },
-    getRoute: function (routeId) {
-      return routes[routeId];
-    }
-  };
+    var routes = {
+        "A": {
+            "frequency": "Every 1 hour",
+            "buildings": ["371", "36/37", "14", "GP-28"]
+        },
+        "B": {
+            "frequency": "Every 1 hour",
+            "buildings": ["371", "GP-28", "13"]
+        },
+        "C": {
+            "frequency": "Every 1 hour",
+            "buildings": ["371", "36/37", "14"]
+        },
+        "D": {
+            "frequency": "Every 1 hour",
+            "buildings": ["36/37", "14", "17/18"]
+        },
+        "E": {
+            "frequency": "Every 1 hour",
+            "buildings": ["36/37", "14", "13"]
+        }
+    };
+
+    var buildings = {
+        "371": ["A", "B", "C"],
+        "36/37": ["A", "C", "D", "E"],
+        "14": ["A", "C", "D", "E"],
+        "GP-28": ["A", "B"],
+        "17/18": ["D"],
+        "13": ["B", "E"]
+    };
+
+    return {
+        routes: function () {
+            return routes;
+        },
+        getRoute: function (routeId) {
+            return routes[routeId].buildings;
+        },
+        geoPositions: function () {
+            return geoPositions;
+        },
+        getGeoPosition: function (buildingId) {
+            return geoPositions[buildingId];
+        },
+        buildings: function () {
+            return buildings;
+        }
+    };
 });
