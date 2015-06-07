0f9f537a9e2859f477d2b2e0f326054b8f869d47
Nikhil-Pahwa <nikhil_pahwa@yahoo.com>
2015-06-07 17:14:08 +0530

    Driver app related changes

--- ferryDriver/www/js/httpService.js
+++ ferryDriver/www/js/httpService.js
@@ -3,6 +3,71 @@ angular.module('myDriverApp').factory('myServices', ['$http', function httpServi
 	var requestUrl = 'http://10.175.174.150:5000/';
 	var routeId = null,
 		timer = null;
+	
+	var routes = {
+        "A": {
+            "371": {
+                "longitude": "x",
+                "latitude": "y"
+            },
+            "36/37": {
+                "longitude": "x",
+                "latitude": "y"
+            },
+            "14": {
+                "longitude": "x",
+                "latitude": "y"
+            },
+            "GP-28": {
+                "longitude": "x",
+                "latitude": "y"
+            }
+        },
+        "B": {
+            "371": {
+
+            },
+            "GP-28": {
+
+            },
+            "13": {
+
+            }
+        },
+        "C": {
+            "371": {
+
+            },
+            "14": {
+
+            },
+            "36/37": {
+
+            }
+        },
+        "D": {
+            "36/37": {
+
+            },
+            "14": {
+
+            },
+            "17/18": {
+
+            }
+        },
+        "E": {
+            "36/37": {
+
+            },
+            "14": {
+
+            },
+            "13": {
+
+            }
+        }
+    };
 		
 	return {
 		getRoutes: function(){
@@ -11,7 +76,10 @@ angular.module('myDriverApp').factory('myServices', ['$http', function httpServi
 		updateLocation: function(dataObj) {
 			console.log(dataObj)
 			return $http.post(requestUrl + 'updateLocation/', dataObj);
-		}
+		},
+        routeInformation: function() {
+            return routes;
+        }
 	}
 	
 }]);
